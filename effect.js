fad97f3ee0bd963322377ac28d58b0e0c45c0ba8
Hiidhan <42371977+Hiidhan@users.noreply.github.com>
2020-05-04 10:57:39 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -173,11 +173,11 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(3000).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+			$("p:nth-child("+i+")").fadeIn('slow').delay(3500);
+			if(i==90){
+				$("p:nth-child(89)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
